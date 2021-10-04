d1d419be8f39ab465d7273fcdea5fc29b0bea208
Ludde <johan.ludde.wessen@gmail.com>
2021-10-04 19:49:25 +0200

    Update example

--- asyncio_hello_world.py
+++ asyncio_hello_world.py
@@ -1,11 +1,21 @@
 import asyncio
 
-# Copied from: https://docs.python.org/3/library/asyncio.html , 2021-10-04
+# Adapted from: https://docs.python.org/3/library/asyncio.html , 2021-10-04
 
-async def main():
+async def hello_world():
     print('Hello ...')
     await asyncio.sleep(1)
     print('... World!')
 
 # Python 3.7+
-asyncio.run(main())
+print("Event loop:")
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(asyncio.gather( 
+hello_world(),
+hello_world(),
+hello_world(),
+hello_world(),
+hello_world(),
+))
+loop.close()
