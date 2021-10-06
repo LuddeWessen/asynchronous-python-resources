83da353a6ce02124f161db178aaab6457a7b65fb
Ludde <johan.ludde.wessen@gmail.com>
2021-10-06 15:50:57 +0200

    Add multiprocessor processpool example

---
+++ multiprocessing_pool_hello_world.py
@@ -0,0 +1,47 @@
+import concurrent.futures
+import time
+
+def hello_world(delay):
+    print(f'Hello ...{delay}')
+    time.sleep(delay)
+    return f'...{delay} sec delayed world!'
+
+
+t1 = time.time()
+n = 5
+delays = [i for i in range(n,0,-1)]
+
+with concurrent.futures.ProcessPoolExecutor() as executor:
+
+
+    results = []
+    # Submit method adds function to be executed, and returns a 'futures' object,
+    # a future object ~encapsulates the execution of our function, and lets us "check on it"
+    for dl in delays:
+        results.append(executor.submit(hello_world, dl))
+
+    # Or equally:
+    #results = [executor.submit(hello_world, dl) for dl in range(n,0,-1)]
+
+    print("Type: ", type(results[0]))
+
+    # Print results, as they arrive
+    for f in concurrent.futures.as_completed(results):
+        print(f.result())
+
+    t2 = time.time()
+    print("Correctly done after %s" % (t2-t1))
+
+
+    # Let's compare to generator function 'map'
+    t1 = time.time()
+
+    # Similarly with "maps", the math object:
+    results = executor.map(hello_world, delays)
+    print("Type: ", type(results))
+
+    for result in results:
+        print(result)
+
+    t2 = time.time()
+    print("Correctly done after %s" % (t2-t1))
